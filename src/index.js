d5a25ea227c006e7b5b58a31238d7ae55f641a7f
darkboat <66820699+darkboat@users.noreply.github.com>
2021-02-21 18:56:48 +0100

    finished README.md

--- src/index.js
+++ src/index.js
@@ -8,20 +8,29 @@ const game = new fruity.Game(canvas); // Initiate Game
 const character = new fruity.Sprite(game, 100, 100, 50, 50); // Create a sprite(character)
 game.Sprites.push(character); // Add the sprite to the draw loop
 
-const characterCollider = new fruity.BoxCollider(character);
-
 game
   .onKeyPress("w", () => {
+    // When "w" is pressed it will move our character up
     character.moveUp(speed);
   })
   .onKeyPress("a", () => {
+    // When "a" is pressed it will move our character to the left
     character.moveLeft(speed);
   })
   .onKeyPress("s", () => {
+    // When "s" is pressed it will move our character down
     character.moveDown(speed);
   })
   .onKeyPress("d", () => {
+    // When "d" is pressed it will move our character to the right
     character.moveRight(speed);
+  })
+  .onKeyPress(" ", () => {
+    // When "spacebar" is pressed it will make our character jump
+    character.moveUp(50);
+    setTimeout(() => {
+      character.moveDown(50);
+    }, 2000);
   });
 
 game.loop(); // Run the game loop
